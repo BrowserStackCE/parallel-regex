85f4eca076b22670bcf3502a8b20b2cfcd286742
somebodysmokein <venky88r@gmail.com>
2025-05-02 14:13:25 +0530

    updated with MashReq app url

--- src/test/java/com/browserstack/perftest/MashReqAppPerf.java
+++ src/test/java/com/browserstack/perftest/MashReqAppPerf.java
@@ -1,7 +1,6 @@
 package com.browserstack.perftest;
 
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.AppiumClientConfig;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import org.openqa.selenium.JavascriptExecutor;
@@ -10,11 +9,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITest;
-import org.testng.ITestContext;
-import org.testng.ITestNGMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import java.net.URL;
 import java.time.Duration;
@@ -53,15 +49,15 @@ public class MashReqAppPerf implements ITest {
 
     }
 
-    //@Test(dataProvider = "testData")
+
     @Test
     public void testApp() throws Exception {
         //int data=1;
         System.out.println("Test run "+ data);
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(30));
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(90));
         WebElement searchElement = (WebElement) wait.until(
-                ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Search Wikipedia")));
+                ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@text=\"Sign In\"]/parent::android.widget.Button")));
 
         searchElement.isDisplayed();
 
